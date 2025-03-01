342549659e352a757967b2762045bbe1b7427df6
Joseph Guadagno <jguadagno@hotmail.com>
2025-03-01 09:03:25 -0700

    Added new Refresh Files

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This contains different settings and configuration items for my Dell 5570
 
+I run the [Refresh-Files.ps1](Refresh-Files.ps1) every once in a while to update all of the files that are in this backup.
 
 ## Software Required
 
--- Refresh-Files.ps1
+++ Refresh-Files.ps1
@@ -5,13 +5,15 @@ $MyDocumentsFolder = [Environment]::GetFolderPath('MyDocuments')
 
 # Get the latest UniGet Bundle
 $UniGetFolder = $MyDocumentsFolder + "\UniGetUi"
+$UniGetBackupName = "UniGet Installed Packages.json"
 $NewestFile = Get-ChildItem -Path $UniGetFolder -Filter "*.ubundle" -File | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-Copy-Item -Path $NewestFile.FullName -Destination "UniGet Installed Packages.json"
-git add $NewestFile.Name
+Copy-Item -Path $NewestFile.FullName -Destination $UniGetBackupName
+git add $UniGetBackupName
 
 # Get the latest PowerShell_profile
 Copy-Item -Path $PROFILE
 git add .
 
 $gitMessage = "Updated files  $(Get-Date)"
-#git commit -M
+git commit -M
+git push
